2041e37483401e066bacc1e24315248482f9ff60
StingraySA <wvermaak@mweb.co.za>
2025-02-11 08:36:19 +0200

    Some tweaks to init.py

--- custom_components/weatherxmlocal/__init__.py
+++ custom_components/weatherxmlocal/__init__.py
@@ -1,20 +1,23 @@
-"""WeatherXM Local integration for Home Assistant."""
+import logging
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers.typing import ConfigType
 
 DOMAIN = "weatherxmlocal"
+PLATFORMS = ["sensor"]
 
-async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up the WeatherXM Local component."""
+_LOGGER = logging.getLogger(__name__)
+
+def setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up the WeatherXM Local integration."""
     return True
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up WeatherXM Local from a config entry."""
     hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = entry.data
+    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
     return True
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Unload a config entry."""
-    hass.data[DOMAIN].pop(entry.entry_id)
-    return True
+def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload WeatherXM Local config entry."""
+    return hass.config_entries.async_unload_platforms(entry, PLATFORMS)
